d074cacfc551a897c19bdcfb702d8aef4f55ff7b
joy_adonia <joy_adonia@yahoo.com>
2016-10-17 16:24:57 +0300

    All classes were changed

---
+++ PSSC/UPT/WpfApplication1/WpfApplication1/Calcul.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1
+{
+    public class Calcul
+    {
+       public  float calculMedieTotala(Situatie situatie)
+        {
+           
+            
+            return 2;
+        }
+      public  float calculNotaFinala(Nota nota,int procent)
+        {   //((nota.Activitate*procent)+nota.Evaluare)/2
+            return 1;
+        }
+    }
+}
---
+++ PSSC/UPT/WpfApplication1/WpfApplication1/Nota.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1
+{
+   public class Nota
+    {   
+        private int _activitate;
+
+        public int Activitate
+        {
+            get { return _activitate; }
+            set { _activitate = value; }
+        }
+        private List<string> _evaluare;
+
+        public List<string> Evaluare
+        {
+            get { return _evaluare; }
+            set { _evaluare = value; }
+        }
+
+    }
+}
